cbdbc96b6e59cce30f5ab02ac3b6aea3c5b02eb7
joppa0808 <de_ballen_man@hotmail.com>
2020-01-26 15:36:58 +0100

    testende

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<bloemenwebshop>'));
+app.use(express.static(__dirname));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/<bloemenwebshop>/index.html'));
+  res.sendFile(path.join(__dirname));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 80);
