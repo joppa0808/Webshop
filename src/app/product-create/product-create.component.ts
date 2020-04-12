7268e923ba55808250a899cdaed248a3c40b666f
joppa0808 <de_ballen_man@hotmail.com>
2020-04-12 11:33:25 +0200

    update26

--- src/app/product-create/product-create.component.ts
+++ src/app/product-create/product-create.component.ts
@@ -33,6 +33,8 @@ export class ProductCreateComponent implements OnInit {
       imagepath: ['', [Validators.required]],
       productbeschrijving: ['', [Validators.required]]
     });
+    this.createForm.setValue({productname: this.product.productname, price: this.product.price, imagepath: this.product.imagepath,
+      productbeschrijving: this.product.productbeschrijving});
   }
   get f() {
     return this.createForm.controls;
